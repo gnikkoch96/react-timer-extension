5adc24cbae188b94f7e08a90fb53c0fcbcd52f33
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2026-08-30 12:37:04 -0700

    fix(vite): update vite config so browser knows how to read dist files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: resolve(__dirname, 'index.html'),
+        background: resolve(__dirname, 'src/background.ts'),
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'background') {
+            return 'src/background.js';
+          }
+          return 'assets/[name]-[hash].js';
+        },
+      },
+    },
+  },
+});
